1d41a297d079d7db3a57dbcfee38aa1c9b8de549
Dania Denisuk <daniadenisuk72@gmail.com>
2024-02-15 17:16:00 +0200

    Working with SegmentedView

---
+++ Future News/Future News/Views/ProgressAlertView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressAlertView.swift
+//  Future News
+//
+//  Created by Danya Denisiuk on 15.02.2024.
+//
+
+import SwiftUI
+
+struct ProgressAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressAlertView()
+}
